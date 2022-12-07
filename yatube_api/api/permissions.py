07fe31ae2d6a16683b8330e6f6d864ba96f0a757
Ольга Жолудева <joludeva.olga@yandex.ru>
2022-12-07 18:13:59 +0300

    Домашка 9-го спринта

---
+++ yatube_api/api/permissions.py
@@ -0,0 +1,15 @@
+from rest_framework import permissions
+
+class AuthorOrReadOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return any([
+            request.method in permissions.SAFE_METHODS,
+            request.user.is_authenticated
+        ])
+    
+    def has_object_permission(self, request, view, obj):
+        return any([
+            request.method in permissions.SAFE_METHODS,
+            obj.author == request.user
+        ])
